c45c61302aaddb24cae82981048c2360f99c00fa
BeomA <beom205@gmail.com>
2019-11-14 04:28:05 +0900

    MainController에 / 로 접근가능하게 수정

--- membership-trading2/src/main/java/com/mt/gmts/controller/MainController.java
+++ membership-trading2/src/main/java/com/mt/gmts/controller/MainController.java
@@ -1,8 +1,32 @@
 package com.mt.gmts.controller;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.java.Log;
 
 @Controller
+@Log
 public class MainController {
 
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		log.info("Welcome home! The client locale is {}." + locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
 }
